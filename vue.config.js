d320692f06e0e8dbcab40d0e2f83765d992a6ce7
s0loparty <izovo@bk.ru>
2021-11-04 17:16:30 +0300

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,4 @@
 module.exports = {
-	publicPath: process.env.NODE_ENV === 'production' ? '/dist/' : '/',
+	publicPath: process.env.NODE_ENV === 'production' ? '/public/' : '/',
 	productionSourceMap: false
 }
